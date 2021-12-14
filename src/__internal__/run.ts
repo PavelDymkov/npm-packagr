2167583b4d53da3d2e0c9cb9086580d3dd492756
Pavel Dymkov <dymkov86@gmail.com>
2021-12-14 14:11:00 +0300

    npm-packagr

--- src/__internal__/run.ts
+++ src/__internal__/run.ts
@@ -1,10 +1,14 @@
 import { spawnSync } from "child_process";
 import { not } from "logical-not";
 
-export function run(command: string, options?: { silent?: boolean }): boolean {
+export function run(
+    command: string,
+    options?: { cd?: string; silent?: boolean },
+): boolean {
     const [name, ...args] = command.trim().split(/\s+/);
 
     const { status } = spawnSync(name, args, {
+        cwd: options?.cd,
         stdio: options?.silent ? "ignore" : "inherit",
     });
 
